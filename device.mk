2ab075db04aedafc3265a24e9005bb1915f41a14
L1ghtzin <lightzin021@gmail.com>
2025-12-29 13:54:14 -0300

    sanders: Update media blobs from LA.UM.9.6.2.r1-04200-89xx.0 * media configs from mido

--- device.mk
+++ device.mk
@@ -350,11 +350,6 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_utils.xml
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
