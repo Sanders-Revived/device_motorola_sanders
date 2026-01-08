b14ae5b88a61995e1cce2508e5440d59118285b4
mikeNG <mkbestas@lineageos.org>
2026-01-08 14:26:39 -0300

    sanders: Include lineage FCM Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,8 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 TARGET_FS_CONFIG_GEN := \
     $(DEVICE_PATH)/configs/config.fs \
