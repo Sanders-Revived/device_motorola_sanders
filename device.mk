6e1c53a1ee7e15fd11a9f6cf5be3b26de6d2055f
npjohnson <johnsonnolen@gmail.com>
2026-01-08 22:57:36 -0300

    sanders: Move to common lights HAL Change-Id: Ib262092d854c3a9cd5e062e943132e35b78885c3

--- device.mk
+++ device.mk
@@ -208,7 +208,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.msm8953
+    android.hardware.light-service.lineage
 
 # Media
 PRODUCT_COPY_FILES += \
