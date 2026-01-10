dda6b6d425887896cbc21c35dda7b85c61537598
mikeNG <mkbestas@lineageos.org>
2026-01-10 17:10:15 -0300

    sanders: Switch to AIDL thermal HAL Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -352,7 +352,7 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
