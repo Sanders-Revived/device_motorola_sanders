7ee8a2caab41669bdec8caed53f03942e2fd1306
L1ghtzin <lightzin021@gmail.com>
2026-09-05 20:13:02 -0300

    sanders: Build vendor.display.config@2.0.vendor

--- device.mk
+++ device.mk
@@ -83,6 +83,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
+    vendor.display.config@2.0.vendor \
     vendor.qti.hardware.memtrack-service \
     gralloc.msm8953 \
     hwcomposer.qcom \
