e29634abff884cf0c3a606fb7fba860a71540f91
L1ghtzin <lightzin021@gmail.com>
2026-09-08 10:54:13 -0300

    sanders: Update QMI, QC RIL, and Radio blobs to LA.UM.10.6.2.r1-02500-89xx.0
    - Update # QMI (diag) and # QMI (shared dependencies) to 10.6.2
    - Drop unused/orphan 32-bit QMI/Radio libraries lacking 32-bit callers
    - Remove DISABLE_CHECKELF/DISABLE_DEPS flags from libdsi_netctrl.so
    - Update # QC RIL binaries and libraries to 10.6.2
    - Update 48 binary blobs, daemons, and configs in # Radio to 10.6.2
    - Add vendor.qti.hardware.radio.lpa@1.2.so needed by libril-qc-hal-qmi.so
    - Complete ELF dependency checks with zero missing dependencies
    - Expose @1.2::IRadioConfig/default and @1.2::IUimLpa in manifest.xml
    - Include extphonelib, extphonelib.xml, and telephony-ext in device.mk
    - Add telephony.cdma and sip.voip permissions to PRODUCT_COPY_FILES
    - Set sim_slots.count=2 and active_modems.max_count=2 in product.prop

--- device.mk
+++ device.mk
@@ -322,10 +322,13 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
+    extphonelib \
+    extphonelib.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml
+    qti_telephony_utils.xml \
+    telephony-ext
 
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
@@ -384,8 +387,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
