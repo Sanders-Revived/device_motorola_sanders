08224cdc7ab8e0c2cccd7370c29eaa7b2e8c9843
L1ghtzin <lightzin021@gmail.com>
2026-09-09 18:52:36 -0300

    sanders: Clean up build flags, properties, and framework configuration
    - BoardConfig: Remove unused audio feature flags and obsolete codec definition
    - BoardConfig: Drop deprecated USE_CAMERA_STUB and TARGET_NO_RPC
    - Properties: Drop obsolete debug.media.codec2 property
    - Properties: Move ro.vendor.fm.use_audio_session and ro.vendor.sensors.smd to vendor properties
    - Overlay: Drop obsolete swipe-up gesture setting and non-existent updatable-media.jar
    - Permissions: Remove unused privapp-permissions-hotword.xml

--- device.mk
+++ device.mk
@@ -346,9 +346,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.sensors.smd=false
-
 # Shims
 PRODUCT_PACKAGES += \
     libgui_shim_vendor \
