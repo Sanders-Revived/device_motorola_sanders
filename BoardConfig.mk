32e7ced8cd5cfde02e97c3038e7bee4299cbebf8
L1ghtzin <lightzin021@gmail.com>
2026-09-23 10:58:58 -0300

    sanders: Update defconfig path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,7 +130,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
-TARGET_KERNEL_CONFIG := sanders_defconfig
+TARGET_KERNEL_CONFIG := vendor/sanders-perf_defconfig
 TARGET_KERNEL_VERSION := 4.19
 
 TARGET_KERNEL_CLANG_COMPILE := true
