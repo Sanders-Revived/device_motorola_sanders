4aa207bc5a5ddd721d5f4efb3be5b18d23c860c9
L1ghtzin <lightzin021@gmail.com>
2026-09-23 10:54:20 -0300

    sanders: Update kernel version to msm-4.19

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,7 +131,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $
 
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 TARGET_KERNEL_CONFIG := sanders_defconfig
-TARGET_KERNEL_VERSION := 4.9
+TARGET_KERNEL_VERSION := 4.19
 
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r522817
